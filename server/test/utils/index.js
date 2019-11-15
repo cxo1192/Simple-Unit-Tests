0e07201fb48887f1e91b9906a43316e4195e75b6
Cameron Ostrom (RIT Student) <cxo1192@ad.rit.edu>
2019-11-15 14:55:19 -0500

    unit test demo

--- server/test/utils/index.js
+++ server/test/utils/index.js
@@ -1,5 +1,38 @@
 // const config = require('../test_config.js');
-// const utils = require('../../src/utils');
+ const utils = require('../../src/utils');
 //
-// const { limits } = config;
+const { limits } = config;
 //
+
+
+    describe('utils', ()=>{
+        describe('add', ()=> {
+            it('should return addition of two numbers', (done) => {
+                const sum = utils.add(10,5);
+                sum.should.equal(15);
+                done();
+            });
+
+            it('should clamp number to minimum', (done) => {
+                const sum = utils.add(limits.MIN-100,1);
+                sum.should.equal(limits.MIN);
+                done();
+            });
+
+
+            it('should clamp number to maximum', (done) => {
+                const sum = utils.add(limits.MAX,1);
+                sum.should.equal(limits.MAX);
+                done();
+            });
+        });
+
+
+        describe('subtract', ()=>{
+            it('should return subtraction of two numbers', (done) => {
+                const dif = utils.sub(10,5);
+                dif.should.equal(5);
+                done();
+            });
+        });
+    });
